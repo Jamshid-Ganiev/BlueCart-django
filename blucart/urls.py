ad548abb00cc90718fe1bc0524ab8b888a5ac493
Jamshid-Ganiev <jamesganiev227@gmail.com>
2023-03-23 00:47:30 +0900

    first commit

---
+++ blucart/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path, include
+from .views import home
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', home, name='home'),
+
+    path('store/', include('store.urls')),
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
