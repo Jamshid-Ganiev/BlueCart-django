9b000a48ff2b8f3b2aeb48c1386999f97a1fc115
Jamshid-Ganiev <jamesganiev227@gmail.com>
2023-03-23 14:10:39 +0900

    Add more features to cart

--- store/views.py
+++ store/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product
 from category.models import Category
+from carts.models import CartItem, Cart
+from carts.views import _cart_id
 
 # Create your views here.
 def store(request, category_slug=None):
@@ -27,9 +29,11 @@ def store(request, category_slug=None):
 def product_detail(request, category_slug, product_slug):
     try:
         single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+        in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request), product=single_product).exists()
     except Exception as error:
         raise error
     context = {
         "single_product": single_product,
+        "in_cart": in_cart,
     }
     return render(request, 'store/product_detail.html', context)
