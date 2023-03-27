7c66f3ffa7dd9afa0bb264dc8b65167b78df35bf
Jamshid-Ganiev <jamesganiev227@gmail.com>
2023-03-27 22:04:48 +0900

    Add Forgot Password and Reset Password features

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,4 @@
-from .views import register, login, logout, activate, dashboard
+from .views import register, login, logout, activate, dashboard, forgotPassword, resetpassword_validate, resetPassword
 from django.urls import path
 
 app_name='accounts'
@@ -8,6 +8,9 @@ urlpatterns = [
     path('logout/', logout, name='logout'),
     path('dashboard/', dashboard, name='dashboard'),
     path('', dashboard, name='dashboard'),
+    path('forgotPassword/', forgotPassword, name='forgotPassword'),
+    path('resetPassword/', resetPassword, name='resetPassword'),
+    path('resetpassword_validate/<uidb64>/<token>/', resetpassword_validate, name='resetpassword_validate'),
     
     path('activate/<uidb64>/<token>/', activate, name='activate'),
 
--- carts/urls.py
+++ carts/urls.py
@@ -4,7 +4,7 @@ from .views import cart, add_cart, remove_cart, remove_cart_item
 app_name = "carts"
 urlpatterns = [
     
-    path('', cart, name="cart"),
+    path('carts/', cart, name="cart"),
     path('add_cart/<int:product_id>/', add_cart, name="add_cart"),
     path('remove_cart/<int:product_id>/<int:cart_item_id>/', remove_cart, name="remove_cart"),
     path('remove_cart_item/<int:product_id>/<int:cart_item_id>/', remove_cart_item, name="remove_cart_item"),
