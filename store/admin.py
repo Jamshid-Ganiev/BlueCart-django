e18492a548599f39880867dac10091cc87cda73e
Jamshid-Ganiev <jamesganiev227@gmail.com>
2023-04-29 20:20:52 +0900

    Add Product Image Gallery

--- store/admin.py
+++ store/admin.py
@@ -1,11 +1,18 @@
 from django.contrib import admin
-from .models import Product, Variation, ReviewRating
+from .models import Product, Variation, ReviewRating, ProductGallery
+import admin_thumbnails
 
 # Register your models here.
+
+@admin_thumbnails.thumbnail('image')
+class ProductGalleryInline(admin.TabularInline):
+    model = ProductGallery
+    extra = 1
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('product_name', 'price', 'category', 'modified_date', 'is_available')
     prepopulated_fields = {'slug': ('product_name',)}
+    inlines = [ProductGalleryInline]
 
 class VariationAdmin(admin.ModelAdmin):
     list_display = ('product', 'variation_category', 'variation_value', 'is_active')
@@ -19,3 +26,4 @@ class ReviewRatingAdmin(admin.ModelAdmin):
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Variation, VariationAdmin)
 admin.site.register(ReviewRating, ReviewRatingAdmin)
+admin.site.register(ProductGallery)
--- store/views.py
+++ store/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from orders.models import OrderProduct
 
 from store.forms import ReviewForm
-from .models import Product, ReviewRating
+from .models import Product, ReviewRating, ProductGallery
 from category.models import Category
 from carts.models import CartItem, Cart
 from carts.views import _cart_id
@@ -62,12 +62,17 @@ def product_detail(request, category_slug, product_slug):
     # Get the reviews
     reviews = ReviewRating.objects.filter(product_id=single_product.id, status=True)
 
+    # Get the product gallery
+    product_gallery = ProductGallery.objects.filter(product_id=single_product.id)
+
+
 
     context = {
         "single_product": single_product,
         "in_cart": in_cart,
         "orderproduct": orderproduct,
         "reviews": reviews,
+        'product_gallery': product_gallery,
     }
     return render(request, 'store/product_detail.html', context)
 
