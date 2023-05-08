5986f24ad679fad557beb38b3249011e7697b30e
Jamshid-Ganiev <jamesganiev227@gmail.com>
2023-05-09 04:07:00 +0900

    add postgres configurations

--- blucart/settings.py
+++ blucart/settings.py
@@ -86,23 +86,23 @@ WSGI_APPLICATION = 'blucart.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
-#     "default": {
-#         "ENGINE": 'django.db.backends.postgresql',
-#         "NAME": env('DB_NAME'),
-#         "HOST": env('DB_HOST'),
-#         "PORT": '5432',
-#         "USER": env('DB_USER'),
-#         "PASSWORD": env('DB_PASSWORD')
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    "default": {
+        "ENGINE": 'django.db.backends.postgresql',
+        "NAME": config('DB_NAME'),
+        "HOST": config('DB_HOST'),
+        "PORT": '5432',
+        "USER": config('DB_USER'),
+        "PASSWORD": config('DB_PASSWORD')
+    }
+}
 
 
 # Password validation
