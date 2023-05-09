623484caacd927ef60c0337a76a0688173d3be99
Jamshid-Ganiev <jamesganiev227@gmail.com>
2023-05-09 13:51:32 +0900

    change static url

--- blucart/settings.py
+++ blucart/settings.py
@@ -144,7 +144,7 @@ MEDIA_ROOT = "media-files"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "static/"
 STATICFILES_DIRS = [ BASE_DIR / 'static' ]
 STATIC_ROOT = 'static-files'
 
